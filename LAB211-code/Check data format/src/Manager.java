135299bba630a1c71f3ea08aa150aec792b7a9d3
BuNnYjUmB <bunnyjumb@gmail.com>
2023-06-09 23:23:14 +0700

    Final

--- LAB211-code/Check data format/src/Manager.java
+++ LAB211-code/Check data format/src/Manager.java
@@ -5,41 +5,61 @@ import java.io.InputStreamReader;
 import Validation.Validate;
 
 class Manager {
-    void displayMenu() {
-        Validate validate = new Validate();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String result = "";
+    private Validate validate;
+    private BufferedReader br;
+
+    public Manager() {
+        validate = new Validate();
+        br = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+    public void displayMenu() {
         System.out.println("===============Validate Program===============");
-        try {
-            System.out.print("Phone number: ");
-            do {
+        validatePhoneNumber();
+        validateDate();
+        validateEmail();
+        System.out.println("Everything is valid");
+    }
+
+    private void validatePhoneNumber() {
+        String result = "";
+        System.out.print("Phone number: ");
+        do {
+            try {
                 String phoneCheck = br.readLine();
                 result = validate.checkInputPhone(phoneCheck);
                 System.out.println(result);
-            } while (!result.isEmpty());
+            } catch (IOException e) {
+                System.out.println("An error occurred while reading input. Please try again.");
+            }
+        } while (!result.isEmpty());
+    }
 
-            System.out.print("Date: ");
-            do {
+    private void validateDate() {
+        String result = "";
+        System.out.print("Date: ");
+        do {
+            try {
                 String dateCheck = br.readLine();
                 result = validate.checkInputDate(dateCheck);
                 System.out.println(result);
-            } while (!result.isEmpty());
+            } catch (IOException e) {
+                System.out.println("An error occurred while reading input. Please try again.");
+            }
+        } while (!result.isEmpty());
+    }
 
-            System.out.print("Email: ");
-            do {
+    private void validateEmail() {
+        String result = "";
+        System.out.print("Email: ");
+        do {
+            try {
                 String emailCheck = br.readLine();
                 result = validate.checkInputEmail(emailCheck);
                 System.out.println(result);
-            } while (!result.isEmpty());
-            System.out.println("Everything is valid");
-        } catch (IOException e) {
-            System.out.println("An error occurred while reading input. Please try again.");
-        } finally {
-            try {
-                br.close();
             } catch (IOException e) {
-                System.out.println("An error occurred while closing the input stream.");
+                System.out.println("An error occurred while reading input. Please try again.");
             }
-        }
+        } while (!result.isEmpty());
     }
 }
